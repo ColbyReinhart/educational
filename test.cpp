64b225a7851257ff1b36ba8850d65e5483d68d9b
Colby Reinhart <colbyreinhart@gmail.com>
2023-03-29 17:52:58 -0400

    Initial commit

---
+++ test.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+int addOne()
+{
+    static int num = 1;
+    num++;
+    return num;
+}
+
+void fooBar()
+{
+    std::cout "Food!" << std::endl;
+}
+
+void doThing()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+}
+
+int main()
+{
+    std::string hello = "Hi world!";
+    int* point = new int(10);
+    std::cout << *point << std::endl;
+    std::cout << addOne() << std::endl;
+    std::cout << addOne() << std::endl;
+    std::cout << addOne() << std::endl;
+    delete point;
+}
